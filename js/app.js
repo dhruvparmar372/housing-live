933c60324a8d3790cffa8f1e0ba0b63b986fdbbc
Dhruv Parmar <dhruv.parmar@housing.com>
2015-10-17 02:50:35 +0530

    audionadded

--- js/app.js
+++ js/app.js
@@ -4,6 +4,32 @@
     var body;
 
     
+
+    var InputBox = function(options){
+        var button = $("<button id='start-search-btn'>Start Search</button>"),
+            recognizer = new webkitSpeechRecognition();
+        
+        var recognizer_result = function(event) {
+            if (event.results.length > 0) {
+                var result = event.results[event.results.length-1];
+                if(result.isFinal) {
+                    console.log(result[0].transcript);
+                }
+            }  
+        };
+
+        recognizer.lang = options.language || "en";
+        recognizer.onresult = recognizer_result;
+        
+        var start_voice_search = function(){
+            recognizer.start();
+        }
+        $(button).bind('click',start_voice_search);
+        $(options.append_to).append(button);
+    }
+    
+
+
     function cache_nodes(){
         body = $('body');
     }
@@ -11,6 +37,8 @@
 
     var initialize = function(){
         cache_nodes();
+
+        var input_button = new InputBox({append_to:"#main-content"});
         console.log('load app');
     }
 
